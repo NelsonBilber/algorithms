8ac6cd1cfce6058cea11e612700bc695d87b94e6
Nelson Rodrigues <>
2018-03-09 00:18:46 +0000

    add spdlog c++ log library

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -4,6 +4,10 @@
 #include "calc.h"
 #include "logger.h"
 
+//@todo
+// pass log to a generic lib or util libs
+// Also add test folder to main app
+
 int main(int argc, char *argv[]) {
   using namespace ModernCppCI;
   Logger::level(LogLevel::info);
